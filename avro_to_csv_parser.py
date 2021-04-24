70ffab5d83686826bcd885774edbf9c1f6e28503
Piyush Ingale <37220902+Piyushvingale@users.noreply.github.com>
2021-04-24 10:30:58 +0530

    Add files via upload

--- avro_to_csv_parser.py
+++ avro_to_csv_parser.py
@@ -1,13 +1,30 @@
 import avro.schema, csv
+from output.schema_classes import SchemaClasses
+from avro import datafile, io
+from output import SpecificDatumReader as MovieReader
 from avro.datafile import DataFileReader
 from avro.io import DatumReader
 
+movie_list = SchemaClasses.hindi.movie.movie_listClass
 schema = avro.schema.parse(open("target/schema/movie_list.avsc", "rb").read())
+
+movie = movie_list()
+object_list = []
+
+with open('hindi_movies.avro', 'rb') as f:
+    reader = datafile.DataFileReader(f, MovieReader(readers_schema=schema))
+    for i in reader:
+        movie = movie_list()
+        movie.movie = i.movie
+        movie.genre = i.genre
+        movie.director = i.director
+        movie.actor = i.actor
+        object_list.append(movie)
+    reader.close()
 
 with open('movies_list_output.csv', 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames = ['Movie', 'Genre', 'Director', 'Actors'])
-    reader = DataFileReader(open('hindi_movies.avro', "rb"), DatumReader())
     writer.writeheader()
-    for movie in reader:
-        writer.writerow({'Movie': movie["movie"], 'Genre': ','.join(movie["genre"]), 'Director': ','.join(movie["director"]), 'Actors': ','.join(movie["actor"])})
+    for movie in object_list:
+        writer.writerow({'Movie': movie.movie, 'Genre': ','.join(movie.genre), 'Director': ','.join(movie.director), 'Actors': ','.join(movie.actor)})
     reader.close()
--- csv_to_avro_parser.py
+++ csv_to_avro_parser.py
@@ -1,11 +1,26 @@
+from output.schema_classes import SchemaClasses
+from avro import datafile, io
+from output import SpecificDatumReader as MovieReader
 import avro.schema, csv
 from avro.datafile import DataFileWriter
 from avro.io import DatumWriter
 
+movie_list = SchemaClasses.hindi.movie.movie_listClass
 schema = avro.schema.parse(open("target/schema/movie_list.avsc", "rb").read())
 
-writer = DataFileWriter(open('hindi_movies.avro', "wb"), DatumWriter(), schema)
+movie = movie_list()
+object_list = []
+
 with open('hindi-movies-list.csv', newline='') as csvfile:
     for row in csv.DictReader(csvfile):
-        writer.append({"movie": row['Movie'], "genre": row['Genre'].split(','), "director": row['Director'].split(','), "actor": row['Actors'].split(',')})
+        movie = movie_list()
+        movie.movie = row["Movie"]
+        movie.genre = row["Genre"]
+        movie.director = row["Director"]
+        movie.actor = row["Actors"]
+        object_list.append(movie)
+
+writer = DataFileWriter(open('hindi_movies.avro', "wb"), DatumWriter(), schema)
+for i in object_list:
+    writer.append({"movie": i.movie, "genre": i.genre.split(','), "director": i.director.split(','), "actor": i.actor.split(',')})
 writer.close()
