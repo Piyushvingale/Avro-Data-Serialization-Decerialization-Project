4cb7416a8bb7720a54c9b151e51c4922ee0f7faf
Piyush Ingale <37220902+Piyushvingale@users.noreply.github.com>
2021-04-28 11:56:53 +0530

    Delete generate_python_class.py

--- generate_python_class.py
+++
@@ -1,9 +0,0 @@
-from avrogen import write_protocol_files 
-
-myfile= "hindi_movie.avpr"
-output_directory = "output"
-
-with open(myfile, 'r') as fh:
-    protocol_json=fh.read().replace('\n', '')
-
-write_protocol_files(protocol_json, output_directory,use_logical_types=False) 
