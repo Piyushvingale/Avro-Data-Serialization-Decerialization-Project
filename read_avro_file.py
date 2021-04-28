54c2c7c80004306985a36c757c4ce80fca9f2822
Piyush Ingale <37220902+Piyushvingale@users.noreply.github.com>
2021-04-28 11:55:29 +0530

    Delete read_avro_file.py

--- read_avro_file.py
+++
@@ -1,7 +0,0 @@
-from avro.datafile import DataFileReader
-from avro.io import DatumReader
-
-reader = DataFileReader(open("hindi_movies.avro", "rb"), DatumReader())
-for movie_record in reader:
-    print(movie_record)
-reader.close()
